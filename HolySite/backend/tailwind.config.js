0da8b47802b96a570680e5579d9c6f1d63f99084
holy-jesus <hj@holy-coder.ru>
2024-09-30 21:28:25 +0500

    Перенёс бэк в `backend`

--- HolySite/backend/tailwind.config.js
+++ HolySite/backend/tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   content: ["./static/css/*.css", "./templates/*.html", "./static/js/*.js"],
   theme: {
-    extend: {colors:{
-      "blue": "#00a6ff",
-      "lightblue": "#69d7f3"
-    }},
+    extend: {
+      colors: {
+        blue: "#00a6ff",
+        lightblue: "#69d7f3",
+      },
+    },
   },
   plugins: [],
-  darkMode: 'class',
-}
+  darkMode: "class",
+};
