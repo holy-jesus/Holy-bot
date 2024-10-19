575a3e8c5fcf03c79bb66aafd36ffb3787f761fd
holy-jesus <hj@holy-coder.ru>
2024-10-19 07:02:12 +0500

    Обновил запускной файл

--- HolyBot/run_twitch_api.py
+++ HolyBot/run_twitch_api.py
@@ -1,11 +1,3 @@
 from twitchapi import TwitchApi
-from dotenv import load_dotenv
-import os
 
-load_dotenv(".env")
-
-
-TwitchApi(
-    client_id=os.getenv("app_id"),
-    secret=os.getenv("app_secret"),
-).start()
+TwitchApi().start()
