5ce5572c30897bc65e5e6a2f8ce4d7247353cc0e
holy-jesus <hj@holy-coder.ru>
2024-10-19 06:31:14 +0500

    Добавил `routes/__init__.py`; Сделал переадресацию при неправильном запросе.

---
+++ HolySite/backend/routes/__init__.py
@@ -0,0 +1,3 @@
+from .auth import auth
+
+routes = (auth,)
--- HolySite/backend/routes/auth.py
+++ HolySite/backend/routes/auth.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
 
 auth = APIRouter()
 
 
 @auth.get("/login")
-async def login():
+async def login(
+    request: Request,
+    code: str = None,
+    scope: str = None,
+    state: str = None,
+):
+    if not all((code, scope, state, state == request.cookies.get("state", None))):
+        return RedirectResponse("/")
     pass
 
 
