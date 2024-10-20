30fee876787c92adbfa520469c5af7f13182ed67
holy-jesus <hj@holy-coder.ru>
2024-10-21 02:26:42 +0500

    Добавил авторизацию в БД

--- HolySite/backend/sessions.py
+++ HolySite/backend/sessions.py
@@ -1,7 +1,10 @@
 import asyncio
+import os
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from kafkaclient import Client
 
-db = AsyncIOMotorClient()
+db = AsyncIOMotorClient(
+    username=os.getenv("mongodb_username"), password=os.getenv("mongodb_password")
+)
 client = Client("website", asyncio.get_event_loop())
