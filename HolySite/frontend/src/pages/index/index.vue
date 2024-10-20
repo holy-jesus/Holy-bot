47b17ea3264a4cfebd15a4ef6771cb7dd887aaa1
holy-jesus <hj@holy-coder.ru>
2024-10-21 02:39:58 +0500

    Сделал state при логине

--- HolySite/frontend/src/pages/index/index.vue
+++ HolySite/frontend/src/pages/index/index.vue
@@ -1,6 +1,15 @@
 <script setup>
 import ChatComponent from "src/pages/index/chat/chat.vue";
 import BackgroundComponent from "src/pages/index/background.vue";
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+const scope = ""
+const state = getCookie("state")
 </script>
 
 <template>
@@ -24,7 +33,7 @@ import BackgroundComponent from "src/pages/index/background.vue";
                 помощник.
             </p>
             <a id="loginButton"
-                href="https://id.twitch.tv/oauth2/authorize?response_type=code&scope={{ scope }}&client_id=f7cx3c3iftgmukun3hkzxkdm267e4l&redirect_uri=https://holy-bot.ru/login&state={{ state }}"
+                :href="'https://id.twitch.tv/oauth2/authorize?response_type=code&scope=' + scope + '&client_id=f7cx3c3iftgmukun3hkzxkdm267e4l&redirect_uri=http://localhost:8000/login&state=' + state"
                 v-if="!loggedIn">
                 <button
                     class="px-4 py-1 text-sm text-white font-semibold rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 transition-all duration-300 bg-gradient-to-tr from-[#69d7f3] to-[#00a6ff] scale-100 hover:scale-105">
